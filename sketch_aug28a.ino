fd26151d326bac88406e055ca520a9e5c5b61778
Lenny Lim <liml3@emc.com>
2015-09-08 14:24:54 -0700

    Initial version

---
+++ sketch_aug28a.ino
@@ -0,0 +1,77 @@
+int buttonPins[6] = { 4, 5, 6, 7, 8, 9 } ;
+unsigned long pin_state[6] = { 0, 0, 0, 0, 0, 0 };
+int keycode[2][6] = { { 0, KEY_UP_ARROW, 0, KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW },
+                      { 0, 'w', 0, 'a', 's', 'd' } };
+
+int ledPin = 3;
+int shift_status = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  int cnt;
+
+  for (cnt = 0; cnt < 6; cnt++) {
+    pinMode(buttonPins[cnt], INPUT_PULLUP);
+    digitalWrite(buttonPins[cnt], HIGH);
+  }
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Keyboard.begin();
+  Serial.begin(9600);
+  Serial.println("Hello, world!");
+}
+
+bool debounce(unsigned long t_now, unsigned long t_prev) {
+  unsigned long diff;
+
+  diff = t_now - t_prev; // need to check for underflow?
+
+  if (diff <= 20) return true;
+  else return false;
+}
+
+void loop() {
+  unsigned long tick_now = millis();
+  int cnt;
+
+  // since we use non zero to indicate pressed state, we need
+  // to handle the edge case where millis() returns 0
+  
+  if (tick_now == 0) tick_now = 1;
+
+  for (cnt = 0; cnt < 6; cnt++) {
+    int key_state = 0;
+
+    // ignore state change for pin if in debounce period
+    if (pin_state[cnt] != 0)
+      if (debounce(tick_now, pin_state[cnt]) == true)
+        continue;
+      
+    if (digitalRead(buttonPins[cnt]) == HIGH) {
+      if (pin_state[cnt] != 0) {
+        if (keycode[shift_status][cnt] != 0) {
+            Keyboard.release(keycode[shift_status][cnt]);
+        }
+        pin_state[cnt] = 0;
+      }
+    } else {
+      if (pin_state[cnt] == 0) {
+//        char outstring[64];
+//        sprintf(outstring, "Key %d pressed", cnt);
+//        Serial.println(outstring);
+        if (keycode[shift_status][cnt] != 0) {
+          Keyboard.press(keycode[shift_status][cnt]);
+        } else {
+          if (cnt == 0) {
+            shift_status = 1 - shift_status;
+            digitalWrite(ledPin, (shift_status == 0 ? LOW : HIGH));
+          }
+        }
+        pin_state[cnt] = tick_now;
+      }
+    }
+  }
+
+  delay(3);
+}
